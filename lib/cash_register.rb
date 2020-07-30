4350072b50897f1d7a286aa6404bec353d504202
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-30 23:59:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
-    @items << title
+    quantity.times{@items << title}
   end
   
   def apply_discount
